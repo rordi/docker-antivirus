a5f993f3ef12313ee39b5a1dd99056530c875f88
Dietrich Rordorf <dr@ediqo.com>
2017-01-17 17:03:08 +0100

    change maldet conf, enhance scanner and scanfile bash scripts

--- assets/scanfile.sh
+++ assets/scanfile.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+now=`date +'%Y-%m-%d %T'`
+echo -n "[${now}]"
+echo -n " "
+
 # scan with maldet first
-maldet -a /data/scan
+maldet -a /data/scan/
+
 # maldet quarantines into /usr/local/maldetect/quarantine, move to /data/quarantine
-for file in "/usr/local/maldetect/quarantine"/* ; do
-    filename=`basename $file`
-    echo "Moving maldet quarantined file to /data/quarantine/${filename}"
-    mv $file "/data/quarantine/${filename}"
-done
+files=$(shopt -s nullglob dotglob; echo /usr/local/maldetect/quarantine/*)
+if (( ${#files} ))
+then
+    for file in "/usr/local/maldetect/quarantine"/* ; do
+        filename=`basename $file`
+        echo "Moving maldet quarantined file to /data/quarantine/${filename}"
+        mv $file "/data/quarantine/${filename}"
+    done
+fi
+
 # additionally, scan with ClamAV
-clamscan -rio --quiet --move /data/quarantine /data/scan
+clamscan -rio --quiet --enable-stats --move /data/quarantine /data/scan
--- assets/scanner.sh
+++ assets/scanner.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
-for file in "/data/queue"/* ; do
-    echo "Processing ${file}"
-    filename=`basename $file`
-    mv $file "/data/scan/${filename}"
-    scanfile > /data/scan/info 2>&1
-    if [ -e "/data/scan/${filename}" ]
-    then
-        mv "/data/scan/${filename}" "/data/ok/${filename}"
-        if [ -e "/data/quarantine/${filename}" ]
+files=$(shopt -s nullglob dotglob; echo /data/queue/*)
+if (( ${#files} ))
+then
+    printf "\nFound files to process"
+    for file in "/data/queue"/* ; do
+        filename=`basename $file`
+        mv $file "/data/scan/${filename}"
+        printf "\nProcessing /data/scan/${filename}"
+        scanfile > /data/scan/info 2>&1
+        if [ -e "/data/scan/${filename}" ]
         then
-            hash=`md5sum "/data/quarantine/${filename}"``
+            printf "\n  --> File ok"
+            mv "/data/scan/${filename}" "/data/ok/${filename}"
+            printf "\n  --> File moved to /data/ok/${filename}"
+            rm /data/scan/info
+        elif [ -e "/data/quarantine/${filename}" ]
+        then
+            printf "\n  --> File quarantined / nok"
+            hash=`md5sum "/data/quarantine/${filename}" | awk '{ print $1 }'`
             mv "/data/quarantine/${filename}" "/data/quarantine/${hash}"
-            mv /data/scan/info "/data/nok/${hash}"
+            printf "\n  --> File moved to /data/quarantine/${hash}"
+            mv "/data/scan/info" "/data/nok/${hash}"
+            printf "\n  --> Scan report moved to /data/nok/${hash}"
         fi
-    fi
-done
+    done
+    printf "\nDone with processing\n"
+fi
