4d6bd18034e3d408f7ca86d1a2da8bf135e62014
Dietrich Rordorf <dr@ediqo.com>
2017-11-05 21:14:24 +0100

    adjust cron job timing

--- assets/install_antivirus.sh
+++ assets/install_antivirus.sh
@@ -3,7 +3,7 @@
 # setup cron to update virus signatures hourly
 cd /usr/local
 crontab -l > tempcrons
-echo "5,15,25,35,45,55 * * * * /usr/bin/freshclam >> /var/log/cron/general.log 2>&1" >> tempcrons
-echo "1,11,21,31,41,51 * * * * /usr/local/sbin/maldet -u -d >> /var/log/cron/general.log 2>&1" >> tempcrons
+echo "5 * * * * /usr/bin/freshclam >> /var/log/cron/general.log 2>&1" >> tempcrons
+echo "10 * * * * /usr/local/sbin/maldet -u -d >> /var/log/cron/general.log 2>&1" >> tempcrons
 crontab tempcrons
 rm tempcrons
